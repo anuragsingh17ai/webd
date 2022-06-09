9d79ba89008e1f7ba690034fac673c7b849b36a8
Anurag Singh <anuragsingh17ai@gmail.com>
2022-06-09 09:26:29 +0100

    2nd commit

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+//for now i know only one set of router and res can be executed at a time.........
+router.get('/hello', function(req, res, next) {
+  res.send("<h1>HI IT'S INTERESTING</h1>");
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
