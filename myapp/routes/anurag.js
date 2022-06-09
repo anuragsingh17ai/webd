c501c51989bd9793dd4005120fa2cb0e53493575
Anurag Singh <anuragsingh17ai@gmail.com>
2022-06-09 09:34:57 +0100

    params

--- myapp/routes/anurag.js
+++ myapp/routes/anurag.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('<h1>Hi anurag Sir</h1>');
+router.get('/:name/:mot', function(req, res, next) {
+  res.send('HI '+req.params.name+" Sir i know it's "+req.params.mot);
 });
 
 module.exports = router;
